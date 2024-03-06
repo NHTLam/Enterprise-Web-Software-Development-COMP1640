b3a7d887d1882f090afe1ef8eebc575cd487cb95
Lại Quang Khải <104859097+khailq0909@users.noreply.github.com>
2024-03-07 00:46:55 +0700

    test layout and component

---
+++ CodeFE_COMP1640/codefe_comp1640/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+return (
+    <div className='header'>
+        <h1 className='title'>
+            GR-News
+        </h1>
+    </div>
+)
+}
+
+export default Header
---
+++ CodeFE_COMP1640/codefe_comp1640/src/layouts/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+
+import Footer from '../components/Footer/Footer'
+
+const layout = () => {
+return (
+    <div>
+        <Header/>
+        <Footer/>
+    </div>
+)
+}
+
+export default layout
