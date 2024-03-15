fcca3de0db5f157fdd0bf7951799ae64a2943c42
wizard123123 <161414716+wizard123123@users.noreply.github.com>
2024-03-16 04:37:59 +0700

    Feedback

---
+++ CodeBE_COMP1640/CodeBE_COMP1640/Controllers/FeedbackController/FeedbackRoute.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using CodeBE_COMP1640.Enums;
+
+namespace CodeBE_COMP1640.Controllers
+{
+    public static class FeedbackRoute
+    {
+        public const string Module = "/api/feedback";
+        public const string GetAllFeedbacks = Module + "/all";
+        public const string GetFeedbackById = Module + "/{id}";
+        public const string CreateFeedback = Module + "/create";
+        public const string UpdateFeedback = Module + "/update/{id}";
+        public const string DeleteFeedback = Module + "/delete/{id}";
+
+        public static Dictionary<string, List<string>> DictionaryPath = new Dictionary<string, List<string>>
+        {
+            { ActionEnum.READ.Name, new List<string> { GetAllFeedbacks, GetFeedbackById } },
+            { ActionEnum.CREATE.Name, new List<string> { CreateFeedback } },
+            { ActionEnum.UPDATE.Name, new List<string> { UpdateFeedback } },
+            { ActionEnum.DELETE.Name, new List<string> { DeleteFeedback } }
+        };
+    }
+}
---
+++ CodeBE_COMP1640/CodeBE_COMP1640/Services/FeedbackS/FeedbackValidator.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using CodeBE_COMP1640.Models;
+
+namespace CodeBE_COMP1640.Services.FeedbackS
+{
+    public class FeedbackValidator
+    {
+        public FeedbackValidator()
+        {
+        }
+    }
+}
