bf1827dc05e6323f8f905e141752ad904a40d23d
lam <105333043+NHTLam@users.noreply.github.com>
2024-03-17 10:50:53 +0700

    Chỉnh lại format

--- CodeBE_COMP1640/CodeBE_COMP1640/Repositories/UOW.cs
+++ CodeBE_COMP1640/CodeBE_COMP1640/Repositories/UOW.cs
@@ -3,36 +3,36 @@ using CodeBE_COMP1640.Models;
 
 namespace CodeBE_COMP1640.Repositories
 {
-        public interface IUOW
+    public interface IUOW
+    {
+        IUserRepository UserRepository { get; }
+        IPermissionRepository PermissionRepository { get; }
+        ICommentRepository CommentRepository { get; }
+        IFeedbackRepository FeedbackRepository { get; }
+
+    }
+    public class UOW : IUOW
+    {
+        private DataContext DataContext;
+        public IUserRepository UserRepository { get; private set; }
+        public IPermissionRepository PermissionRepository { get; private set; }
+        public ICommentRepository CommentRepository { get; private set; }
+        private IFeedbackRepository _feedbackRepository;
+
+        public UOW(DataContext DataContext)
         {
-                IUserRepository UserRepository { get; }
-                IPermissionRepository PermissionRepository { get; }
-                ICommentRepository CommentRepository { get; }
-                IFeedbackRepository FeedbackRepository { get; }
+            this.DataContext = DataContext;
+            UserRepository = new UserRepository(DataContext);
+            PermissionRepository = new PermissionRepository(DataContext);
+            CommentRepository = new CommentRepository(DataContext);
 
         }
-        public class UOW : IUOW
+        public IFeedbackRepository FeedbackRepository
         {
-                private DataContext DataContext;
-                public IUserRepository UserRepository { get; private set; }
-                public IPermissionRepository PermissionRepository { get; private set; }
-                public ICommentRepository CommentRepository { get; private set; }
-                private IFeedbackRepository _feedbackRepository;
-
-                public UOW(DataContext DataContext)
-                {
-                        this.DataContext = DataContext;
-                        UserRepository = new UserRepository(DataContext);
-                        PermissionRepository = new PermissionRepository(DataContext);
-                        CommentRepository = new CommentRepository(DataContext);
-
-                }
-                public IFeedbackRepository FeedbackRepository
-                {
-                        get
-                        {
-                                return _feedbackRepository ??= new FeedbackRepository(DataContext);
-                        }
-                }
+            get
+            {
+                return _feedbackRepository ??= new FeedbackRepository(DataContext);
+            }
         }
+    }
 }
