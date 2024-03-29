4089a3d0f63a0634cfd99492bd62d22e4e5f7181
Lại Quang Khải <104859097+khailq0909@users.noreply.github.com>
2024-03-29 23:24:42 +0700

    pagination

--- CodeFE_COMP1640/codefe_comp1640/src/pages/Home.jsx
+++ CodeFE_COMP1640/codefe_comp1640/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link, useNavigate } from "react-router-dom"
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-
+import Pagination from '../components/Pagination';
 const API_BASE = process.env.REACT_APP_API_KEY;
 
 const Home = () => {
@@ -26,13 +26,13 @@ const Home = () => {
             }
         }
         navigate("/");
-    },[])
+    }, [])
     localStorage.setItem("user_id", userId);
     return (
         <div className="container">
             <div className="row">
-            <h1 className='mt-5'>Topic</h1>
-            <hr></hr>
+                <h1 className='mt-5'>Topic</h1>
+                <hr></hr>
                 <div className="col">
                     <Link to={"/topic/view"}>
                         <div className="card">
@@ -53,62 +53,9 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-
             <h1 className='mt-5'>Top contribution</h1>
             <hr></hr>
-            <div className="row mt-4">
-                <div className="col">
-                    <div className="card">
-                        <img src="https://www.analyticsinsight.net/wp-content/uploads/2021/07/Technology-Can-Boost-Your-Business-Productivity.jpg" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <img src="https://www.analyticsinsight.net/wp-content/uploads/2021/07/Technology-Can-Boost-Your-Business-Productivity.jpg" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <img src="https://www.analyticsinsight.net/wp-content/uploads/2021/07/Technology-Can-Boost-Your-Business-Productivity.jpg" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="row mt-4">
-                <div className="col">
-                    <div className="card">
-                        <img src="https://www.analyticsinsight.net/wp-content/uploads/2021/07/Technology-Can-Boost-Your-Business-Productivity.jpg" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <img src="https://www.analyticsinsight.net/wp-content/uploads/2021/07/Technology-Can-Boost-Your-Business-Productivity.jpg" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card">
-                        <img src="https://www.analyticsinsight.net/wp-content/uploads/2021/07/Technology-Can-Boost-Your-Business-Productivity.jpg" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Pagination itemsPerPage={4} />
         </div>
     )
 }
