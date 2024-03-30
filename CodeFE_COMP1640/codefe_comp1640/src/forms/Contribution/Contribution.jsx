f8bc310342185bf89bf83759d2b317487c76d63f
Lại Quang Khải <104859097+khailq0909@users.noreply.github.com>
2024-03-30 15:19:24 +0700

    change ui home page

--- CodeFE_COMP1640/codefe_comp1640/src/forms/Contribution/Contribution.jsx
+++ CodeFE_COMP1640/codefe_comp1640/src/forms/Contribution/Contribution.jsx
@@ -8,11 +8,19 @@ function Contribution({ currentItems }) {
             {
                 currentItems.map((item, index) => {
                     return (
-                        <div className="col-3 p-1 mb-3">
-                            <div className="card w-100">
-                                <img src="https://www.analyticsinsight.net/wp-content/uploads/2021/07/Technology-Can-Boost-Your-Business-Productivity.jpg" className="card-img-top" alt="..." />
-                                <div className="card-body">
-                                    <p className="card-text">{item.content}</p>
+                        <div className="mb-3 w-100">
+                            <div class="card w-100">
+                                <div class="row g-0">
+                                    <div class="col-md-4">
+                                        <img src="https://www.analyticsinsight.net/wp-content/uploads/2021/07/Technology-Can-Boost-Your-Business-Productivity.jpg" class="img-fluid rounded-start" alt="..."/>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Card title</h5>
+                                            <p class="card-text">{item.content}</p>
+                                            <p class="card-text"><small class="text-muted">See moree ...</small></p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
